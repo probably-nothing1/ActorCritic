8798c89f75e4723d012b0ecbd8932cd24ae2daa1
gasiortomasz <gasior.tt@gmail.com>
2020-08-13 10:27:39 +0200

    Add simulation step in main loop

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-# from models import create_actor, create_critic
+import gym
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
@@ -6,52 +7,70 @@ from torch.nn import Linear
 
 
 class Actor(nn.Module):
-    def __init__(self):
-        self.layer1 = Linear(4, 32)
+    def __init__(self, observation_dim, action_dim):
+        super().__init__()
+        self.layer1 = Linear(observation_dim, 32)
         self.layer2 = Linear(32, 32)
         self.layer3 = Linear(32, 32)
-        self.layer4 = Linear(32, 2)
+        self.layer4 = Linear(32, action_dim)
 
     def forward(self, observation):
         x = self.layer1(observation)
-        x = F.tanh(x)
-        x = self.layer2(observation)
-        x = F.tanh(x)
-        x = self.layer3(observation)
-        x = F.tanh(x)
-        logits = self.layer4(observation)
-        policy = Categorical(logits)
+        x = torch.tanh(x)
+        x = self.layer2(x)
+        x = torch.tanh(x)
+        x = self.layer3(x)
+        x = torch.tanh(x)
+        logits = self.layer4(x)
+        policy = Categorical(logits=logits)
         action = policy.sample()
-        return logits, policy, action
+        return action
 
 
 class Critic(nn.Module):
-    def __init__(self):
-        self.layer1 = Linear(4, 32)
+    def __init__(self, observation_dim):
+        super().__init__()
+        self.layer1 = Linear(observation_dim, 32)
         self.layer2 = Linear(32, 32)
         self.layer3 = Linear(32, 32)
-        self.layer4 = Linear(32, 2)
+        self.layer4 = Linear(32, 1)
 
     def forward(self, observation):
         x = self.layer1(observation)
-        x = F.tanh(x)
-        x = self.layer2(observation)
-        x = F.tanh(x)
-        x = self.layer3(observation)
-        value = F.tanh(x)
+        x = torch.tanh(x)
+        x = self.layer2(x)
+        x = torch.tanh(x)
+        x = self.layer3(x)
+        x = torch.tanh(x)
+        value = self.layer4(x)
         return value
 
 
 if __name__ == "__main__":
     # create wandb
     # create env
-    actor = Actor()
-    critic = Critic()
+    env = gym.make("CartPole-v0")
+    observation_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    actor = Actor(observation_dim, action_dim)
+    critic = Critic(observation_dim)
     # create exp buffer
     # create optimizers and train functions
 
-    # train loop:
-    #   1. simulate data
-    #   2. train models
-    #   3. log data
-    pass
+    for epoch in range(10):
+        #   1. simulate data
+        done = False
+        o = env.reset()
+        while not done:
+            with torch.no_grad():
+                # o = torch.from_numpy(o)
+                o = torch.as_tensor(o, dtype=torch.float32)
+                a = actor(o)
+                v = critic(o)
+                a, v = a.numpy(), v.numpy()
+                print(f"Picked action {a}. Value of state: {v}")
+                next_o, reward, done, info = env.step(a)
+                print(f"Next obs {next_o}\n reward {reward}\n done {done}\n info {info}\n")
+                o = next_o
+        #   2. train models
+        #   3. log data
